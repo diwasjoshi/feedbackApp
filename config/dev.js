a62d86a5b5080e6b1fafab4efd32ad4e0e503c7a
Diwas Joshi <dj.dij123@gmail.com>
2017-10-20 14:38:07 +0530

    config for dev and prod

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+        "465923578637-07cn2lsu28iuvisl4402fpvd7v10bt3e.apps.googleusercontent.com",
+    googleClientSecret: "g5q-z1d2YGPBijroubax-Rpo",
+    dbURI: "mongodb://dij:1234@ds251845.mlab.com:51845/feedbackapp-dev",
+    cookieKey: "12345"
+};
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.googleClientID,
+    googleClientSecret: process.env.googleClientSecret,
+    dbURI: process.env.dbURI,
+    cookieKey: process.env.cookieKey
+};
