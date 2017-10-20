74618d1cc8ee13566f7fd0e5d74b74456a69c549
Diwas Joshi <dj.dij123@gmail.com>
2017-10-20 14:37:09 +0530

    accounts routes added

---
+++ routes/accounts.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+router.get(
+    '/google/',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google')
+)
+
+
+module.exports = router;
