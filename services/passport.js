3dd976904709054b5c249b64531a52d6ed78798e
Diwas Joshi <dj.dij123@gmail.com>
2017-10-20 14:36:45 +0530

    passport service

---
+++ services/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("user");
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback",
+            proxy: true
+        },
+        (accessToken, profileToken, profile, done) => {
+            var newUser = new User({ googleIdid: profile.id });
+            newUser.save();
+        }
+    )
+);
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
